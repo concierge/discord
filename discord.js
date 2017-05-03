c4603afed8a73ba2e6f9e82550f26bd4f7e85723
Matthew Knox <matthew@makereti.co.nz>
2017-05-03 16:07:01 +1200

    Improvements

--- discord.js
+++ discord.js
@@ -1,237 +1,159 @@
-let discord = require('discord.js'),
-    request = require('request'),
-    callback = null,
-    api = null,
-    bot = null,
-    localAvatarB64 = null,
-    localAvatarBuffer = null,
-    userAvatarB64 = null,
+const discord = require('discord.js'),
+    request = require('request');
 
-    lookUpChannel = function(threadId) {
-        let channel = null;
+class DiscordIntegrationApi extends shim {
+    constructor (commandPrefix, bot) {
+        super(commandPrefix);
+        this._bot = bot;
+    }
 
-        for (let key in bot.channels) {
-            if (!(key && key.trim().length > 0 && !isNaN(key))) {
-                continue;
-            }
-            channel = bot.channels[key];
-            if (channel.id === threadId) {
-                return channel;
-            }
-        }
-        for (let pKey in bot.privateChannels) {
-            if (!(pKey && pKey.trim().length > 0 && !isNaN(pKey))) {
-                continue;
-            }
-            channel = bot.privateChannels[pKey];
-            if (channel.id === threadId) {
-                return channel;
-            }
-        }
-
-        return channel;
-    },
-
-    getUsers = function() {
-        let users = {},
-            user;
-
-        for (let key in bot.users) {
-            if (!(key && key.trim().length > 0 && !isNaN(key))) {
-                continue;
-            }
-            user = bot.users[key];
-            users[user.id] = {
-                name: user.username,
-                id: user.id,
-                tag: '@' + user.username + '#' + user.discriminator
-            };
-        }
-        return users;
-    },
-
-    sentenceSplitter = function(message) {
-        const charLimit = 2000;
-        let spl = message.split('\n'),
-            messages = [''],
-            messagesIndex = 0;
-
-        for (let i = 0; i < spl.length; i++) {
-            while (spl[i].length > charLimit) {
-                let numChars = charLimit - messages[messagesIndex].length;
-                let chars = spl[i].substr(0, numChars);
-                spl[i] = spl[i].substr(numChars);
-                messages[messagesIndex] += chars;
-                messagesIndex++;
-                messages[messagesIndex] = '';
-            }
-            if (messages[messagesIndex].length + spl[i].length > charLimit) {
-                messagesIndex++;
-                messages[messagesIndex] = '';
-                i--;
-            }
-            else {
-                messages[messagesIndex] = messages[messagesIndex] + spl[i] + '\n';
-            }
-        }
-
-        return messages;
-    },
-
-    sendTyping = function(threadId) {
-        //let channel = lookUpChannel(threadId);
-        bot.channels.get(threadId).startTyping();
-    },
-
-    stopTyping = function(threadId) {
-        //let channel = lookUpChannel(threadId);
-        bot.channels.get(threadId).stopTyping();
-    },
-
-    addMentions = function (message) {
-        let users = getUsers();
+    _toggleMentions (message, add) {
+        const users = this.getUsers(),
+            property = add ? 'name' : 'tag';
+        
         for (let value in users) {
-            let index = message.indexOf(users[value].name);
+            const index = message.indexOf(users[value][property]),
+                replace = add ? `<@${users[value].id}>` : users[value].name;
             if (index > 0) {
-                message = message.substr(0, index) + '<@' + users[value].id + '>' + message.substr(index + users[value].name.length);
+                message = message.substr(0, index) + replace + message.substr(index + users[value][property].length);
             }
         }
-        return message;
-    },
+        return message;    
+    }
 
-    removeMentions = function(message) {
-        let users = getUsers();
-        for (let value in users) {
-            let index = message.indexOf(users[value].tag);
-            if (index > 0) {
-                message = message.substr(0, index) + users[value].name + message.substr(index + users[value].tag.length);
-            }
-        }
-        return message;
-    },
+    _stopTyping (threadId) {
+        this._bot.channels.get(threadId).stopTyping();
+    }
 
-    sendSingleMessage = function(message, threadId, channel, callback) {
-        stopTyping(threadId);
-        //bot.sendMessage(channel, message, callback);
-        bot.channels.get(threadId).sendMessage(message).then(message => callback).catch(console.error);
-    },
+    _sendSingleMessage (message, threadId, channel, callback) {
+        this._stopTyping(threadId);
+        channel.sendMessage(message).then(message => callback).catch(console.error);
+    }
 
-    sendMultipleMessages = function(messageList, threadId, channel) {
-        if (!messageList || messageList.length <= 0) {
+    _sendMultipleMessages (messageList, threadId, channel) {
+        if (messageList.length === 0) {
             return;
         }
-        let message = messageList[0];
-        messageList.splice(0, 1);
-        sendSingleMessage(message, threadId, channel, sendMultipleMessages.bind(this, messageList, threadId, channel));
-    },
+        const message = messageList.splice(0, 1);
+        this._sendSingleMessage(message, threadId, channel, this._sendMultipleMessages.bind(this, messageList, threadId, channel));
+    }
+    
+    _destroy () {
+        if (this._bot) {
+            this._bot.destroy();
+            this._bot = null;
+        }
+    }
 
-    sendMessage = function(message, threadId) {
-        let channel = lookUpChannel(threadId);
-        message = addMentions(message);
-        message = sentenceSplitter(message);
-        sendMultipleMessages(message, threadId, channel);
-    },
+    getUsers () {
+        const users = {};
+        for (let key of this._bot.users.keys()) {
+            if (!(key && key.trim().length > 0 && !isNaN(key))) {
+                continue;
+            }
+            const user = this._bot.users.get(key);
+            user.name = user.username;
+            user.tag = `@${user.username}#${user.discriminator}`;
+            users[user.id] = user;
+        }
+        return users;
+    }
+    
+    sendTyping (threadId) {
+        this._bot.channels.get(threadId).startTyping();
+    }
 
-    sendFile = function(type, file, description, threadId) {
-        let channel = lookUpChannel(threadId);
-        stopTyping(threadId);
-        bot.sendFile(channel, file, description);
-    },
+    sendMessage (message, threadId) {
+        const channel = this._bot.channels.get(threadId);
+        message = this._toggleMentions(message, true);
+        const messages = shim._chunkMessage(message, 2000);
+        this._sendMultipleMessages(messages, threadId, channel);
+    }
 
-    setTitle = function(title, threadId) {
-        let channel = lookUpChannel(threadId);
-        bot.setChannelName(channel, title);
-    },
+    sendFile (type, file, description, threadId) {
+        const channel = this._bot.channels.get(threadId);
+        this._stopTyping(threadId);
+        this._bot.sendFile(channel, file, description);
+    }
 
-    recMessage = function(message) {
-        let event = shim.createEvent(message.channel.id, message.author.id, message.author.username, removeMentions(message.cleanContent));
-        callback(api, event);
-    },
+    setTitle (title, threadId) {
+        const channel = this._bot.channels.get(threadId);
+        this._bot.setChannelName(channel, title);
+    }
+};
 
-    initialiseBot = function(token) {
-        bot = new discord.Client( {
+class DiscordIntegration {
+    constructor () {
+        this._localAvatarBuffer = null;
+        this._callback = null;
+        this._api = null;
+    }
+
+    _getAvatar (complete) {
+        request(this.config.avatarUrl, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                this._localAvatarBuffer = new Buffer(body);
+            }
+            complete();
+        });
+    }
+
+    _initialiseBot (token) {
+        const bot = new discord.Client({
             autoReconnect: true,
             forceFetchUsers: true
         });
 
-        bot.on('ready', function() {
-            if (exports.config.avatarUrl && bot.user.avatarURL) {
-                request(bot.user.avatarURL,
-                    function(error, response, body) {
-                        if (!error && response.statusCode === 200) {
-                            userAvatarB64 = /*'data:' +
-                                response.headers['content-type'] +
-                                ';base64,' +*/
-                                new Buffer(body).toString('base64');
-                            if (userAvatarB64 != localAvatarB64) {
-                                console.debug($$`Changing avatar...`);
-                                bot.user.setAvatar(localAvatarBuffer);
-                            }
-                        }
-                    });
+        bot.on('ready', () => {
+            if (this._avatarUrl !== this.config.avatarUrl) {
+                this._getAvatar(() => {
+                    console.debug($$`Changing avatar...`);
+                    this._avatarUrl = this.config.avatarUrl;
+                });
             }
-            if (exports.config.name && bot.user.username != exports.config.name) {
+
+            if (this.config.name && bot.user.username !== this.config.name) {
                 console.debug($$`Changing username...`);
-                bot.user.setUsername(exports.config.name);
+                bot.user.setUsername(this.config.name);
             }
             console.debug($$`Discord is ready`);
         });
 
-        bot.on('message', function(message) {
+        bot.on('message', message => {
             if (bot.user.id !== message.author.id) {
-                recMessage(message);
+                const event = shim.createEvent(message.channel.id, message.author.id, message.author.username, this._api._toggleMentions(message.cleanContent, false));
+                if (this._callback) {
+                    this._callback(this._api, event);
+                }
             }
         });
         
         bot.login(token);
-    };
-
-exports.start = function(cb) {
-    let functions = {
-            sendMessage: sendMessage,
-            sendFile: sendFile,
-            sendTyping: sendTyping,
-            setTitle: setTitle,
-            getUsers: getUsers,
-            commandPrefix: exports.config.commandPrefix
-        },
-        token = null;
-
-    callback = cb;
-    api = shim.createIntegration(functions);
-
-    if (!exports.config.token) {
-        console.error($$`A token must be provided`);
-        return;
+        return bot;
     }
 
-    token = exports.config.token;
+    start (callback) {
+        if (!this.config.token) {
+            return console.error($$`A token must be provided`);
+        }
 
-    if (exports.config.avatarUrl) {
-        request(exports.config.avatarUrl,
-            function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    localAvatarBuffer = new Buffer(body);
-                    localAvatarB64 = /*'data:' +
-                        response.headers['content-type'] +
-                        ';base64,' +*/
-                        new Buffer(body).toString('base64');
-                    initialiseBot(token);
-                }
-            });
+        if (!this.config.commandPrefix) {
+            this.config.commandPrefix = '!';
+        }
+
+        this._callback = callback;
+        this._api = new DiscordIntegrationApi(this.config.commandPrefix, this._initialiseBot(this.config.token));
     }
-    else {
-        initialiseBot(token);
+
+    stop () {
+        this._api._destroy();
+        this._api = null;
+        this._callback = null;
+    }
+
+    getApi () {
+        return this._api;
     }
 };
 
-exports.getApi = function() {
-    return api;
-};
-
-exports.stop = function() {
-    if (bot !== null) {
-        bot.destroy();
-    }
-    callback = null;
-};
+module.exports = new DiscordIntegration();
